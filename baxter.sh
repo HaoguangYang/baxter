ad8fe3130b60894c1506addb7a6f9fc76826809b
Haoguang Yang <yang1510@purdue.edu>
2022-01-04 12:11:51 -0500

    compatibility with ROS noetic

--- baxter.sh
+++ baxter.sh
@@ -23,11 +23,11 @@ baxter_hostname="baxter_hostname.local"
 
 # Set *Either* your computers ip address or hostname. Please note if using
 # your_hostname that this must be resolvable to Baxter.
-your_ip="192.168.XXX.XXX"
+your_ip="127.0.0.1"
 #your_hostname="my_computer.local"
 
 # Specify ROS distribution (e.g. indigo, hydro, etc.)
-ros_version="indigo"
+ros_version="noetic"
 #-----------------------------------------------------------------------------#
 
 tf=$(mktemp)
